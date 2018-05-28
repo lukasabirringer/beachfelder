d2b1d0d76a517f3c508f9a38f3eb6cb5fd3fbe85
Fabian <pecherfabian@gmail.com>
2018-05-28 22:15:09 +0200

    styled cookie disclaimer

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,10 @@
-<div class="js-cookie-consent cookie-consent">
+<ul class="js-cookie-consent cookie-consent notification notification--light notification--bottom">
+	<li class="notification__item">
+		<span class="notification__icon notification__icon--light" data-feather="info"></span>
+		<p class="cookie-consent__message notification__text notification__text--light">{!! trans('cookieConsent::texts.message') !!}</p>
 
-    <span class="cookie-consent__message">
-        {!! trans('cookieConsent::texts.message') !!}
-    </span>
-
-    <button class="js-cookie-consent-agree cookie-consent__agree">
-        {{ trans('cookieConsent::texts.agree') }}
-    </button>
-
-</div>
+		<button class="button-secondary notification__button">
+			<span class="js-cookie-consent-agree cookie-consent__agree button-secondary__label">{{ trans('cookieConsent::texts.agree') }}</span>
+		</button>
+	</li>
+</ul>
