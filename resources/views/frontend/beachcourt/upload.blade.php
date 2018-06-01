a7e3160c39390e999c49583b688aca53241a03fd
Fabian <pecherfabian@gmail.com>
2018-06-02 01:12:23 +0200

    changed layout of upload page

--- resources/views/frontend/beachcourt/upload.blade.php
+++ resources/views/frontend/beachcourt/upload.blade.php
@@ -39,31 +39,19 @@
 		</div>
 
 		<div class="row">
-			<div class="column column--12 column--m-5 -spacing-a">
-				<img src="{{url('/')}}/images/slide-image-01-retina.jpg" class="image image--max-width image--shadow">
-			</div>
-			<div class="column column--12 column--m-7 -spacing-a">
-				<h4 class="-typo-headline-03 -text-color-green">1. Gesamtansicht der Anlage</h4>
+			<div class="column column--12 column--m-4">
+				<img src="{{url('/')}}/images/slide-image-01-retina.jpg" class="image image--max-width image--shadow -spacing-c">
+				<h4 class="-typo-headline-04 -text-color-gray-01 -spacing-d">1. Gesamtansicht der Anlage</h4>
 				<p class="-typo-copy -text-color-gray-01 -spacing-c">Auf dem ersten Bild sollte die Anlage in einem Übersichtsbild aufgenommen werden – gern auch von einem erhöhten Standpunkt.</p>
 			</div>
-		</div>
-
-		<div class="row">
-			<div class="column column--12 column--m-5 -spacing-a">
-				<img src="{{url('/')}}/images/slide-image-02-retina.jpg" class="image image--max-width image--shadow">
-			</div>
-			<div class="column column--12 column--m-7 -spacing-a">
-				<h4 class="-typo-headline-03 -text-color-green">2. Detailbild Pfostenanlage und Netz</h4>
+			<div class="column column--12 column--m-4">
+				<img src="{{url('/')}}/images/slide-image-02-retina.jpg" class="image image--max-width image--shadow -spacing-c">
+				<h4 class="-typo-headline-04 -text-color-gray-01 -spacing-d">2. Detailbild Pfostenanlage und Netz</h4>
 				<p class="-typo-copy -text-color-gray-01 -spacing-c">Auf dem zweiten Bild sollte die Pfostenanlage und das Netz, gegebenenfalls mit Antennen, aufgenommen werden.</p>
 			</div>
-		</div>
-
-		<div class="row">
-			<div class="column column--12 column--m-5 -spacing-a">
-				<img src="{{url('/')}}/images/slide-image-03-retina.jpg" class="image image--max-width image--shadow">
-			</div>
-			<div class="column column--12 column--m-7 -spacing-a">
-				<h4 class="-typo-headline-03 -text-color-green">3. Detailbild Sand und Courtline</h4>
+			<div class="column column--12 column--m-4">
+				<img src="{{url('/')}}/images/slide-image-03-retina.jpg" class="image image--max-width image--shadow -spacing-c">
+				<h4 class="-typo-headline-04 -text-color-gray-01 -spacing-d">3. Detailbild Sand und Courtline</h4>
 				<p class="-typo-copy -text-color-gray-01 -spacing-c">Auf dem dritten Bild sollte die Courtline aufgenommen werden. Damit bekommen deine Beachfreunde auch einen Eindruck von der Qualität des Sandes.</p>
 			</div>
 		</div>
@@ -86,10 +74,16 @@
 			</div>
 		</div>
 
+		<div class="row">
+			<div class="column column--12 -spacing-d">
+				<p class="-typo-copy -text-color-gray-01">Die Bilder sollten eine Auflösung von mindestens 600 x 400 px haben</p>
+			</div>
+		</div>
+
 		<form id="form1" action="{{ url('/beachvolleyballfeld/upload') }}" method="post" enctype="multipart/form-data">
 			{{ csrf_field() }}
 	  	<div class="row">
-	  		<div class="column column--12 column--m-3">
+	  		<div class="column column--12 column--m-4">
 	  			<label class="input-upload -spacing-a">
 	  				<input type="file" name="photos[]" id="gallery-photo-add" class="input-upload__field" multiple />	
 	  				<input type="hidden" name="beachcourtId" value="{{ $beachcourt->id }}">
@@ -98,29 +92,27 @@
 	  					<span class="input-upload__text">Bilder auswählen</span>
 	  				</span>
 	  			</label>
-	  			<div class="gallery -spacing-b" style="border: 1px #efefef solid; border-radius: 4px; padding: 1.25rem; display: none;"></div>
+	  			
 
 	  			@if ($errors->has('photos'))
 	  				<div class="alert alert-danger">{{ $errors->first('photos', ':message') }}</div>
 	  			@endif
 	  		</div>
-		  	<div class="column column--12 column--m-9">
-		  		
+		  	<div class="column column--12 column--m-8">
+		  		<div class="gallery -spacing-b" style="border: 1px #efefef solid; border-radius: 4px; padding: .625rem; display: none;">
+		  		</div>
 		  	</div>
 		  </div>
+
 		  <div class="row">
-		  	<div class="column column--12">
-		  		<label class="input-toggle -spacing-d">
+		  	<div class="column column--12 column--s-6">
+		  		<label class="input-toggle -spacing-a">
 		  		  <input type="hidden" class="input-toggle__hidden" name="contestParticipation" value="0">
 		  		  <input type="checkbox" class="input-toggle__field contestParticipation" name="contestParticipation" value="0">
 		  		  <span class="input-toggle__switch"></span>
 		  		  <span class="input-toggle__label">Ja, ich möchte an der Verlosung der 200 Preise teilnehmen</span>
 		  		</label>
 		  	</div>
-		  </div>
-
-		  <div class="row">
-		  	<div class="column column--12 column--s-6"></div>
 		  	<div class="column column--12 column--s-6">
 		  		<button class="button-primary -spacing-a">
 		  			<span class="button-primary__label">Absenden</span>
@@ -129,6 +121,13 @@
 		  	</div>
 		  </div>
 		</form>
+		<div class="row">
+			<div class="column column--12 -spacing-a">
+				<p class="-typo-copy -typo-copy--small -text-color-gray-01">
+					Rechtlicher Hinweis: Mit dem Upload der Bilder überträgst du die Nutzungsrechte an den hochgeladenen Bilder an den Betreiber der Website beachfelder.de. Bitte beachte, dass die Auswahl der Bilder von beachfelder.de vorgenommen wird. Es besteht kein Anspruch auf Veröffentlichung deiner Bilder. 
+				</p>
+			</div>
+		</div>
 	</div> <!-- .content__main -->
 <!-- <button onclick="$('#gallery-photo-add').val(''); $('.gallery').empty();">clear</button> -->
 
@@ -169,7 +168,7 @@
 						var reader = new FileReader();
 
 						reader.onload = function(event) {
-							$($.parseHTML('<img class="image image--max-width">')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+							$($.parseHTML('<img class="image" style="width: 150px; margin-right: 5px;">')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
 						}
 
 						reader.readAsDataURL(input.files[i]);
